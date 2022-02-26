bb5634665f15ac2684db8c79895d3a7625a2c534
Tomáš Linhart <pasmen@gmail.com>
2022-02-26 20:52:34 +0100

    Remove logging to console from bundle

--- config/webpack.prod.js
+++ config/webpack.prod.js
@@ -1,10 +1,21 @@
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const InlineChunkHtmlPlugin = require("react-dev-utils/InlineChunkHtmlPlugin");
+const TerserPlugin = require("terser-webpack-plugin");
 const { merge } = require("webpack-merge");
 const common = require("./webpack.common.js");
 
 module.exports = merge(common, {
   mode: "production",
   plugins: [new InlineChunkHtmlPlugin(HtmlWebpackPlugin, [/main/])],
+  optimization: {
+    minimize: true,
+    minimizer: [
+      new TerserPlugin({
+        terserOptions: {
+          compress: { drop_console: true },
+        },
+      }),
+    ],
+  },
   devtool: "source-map",
 });
