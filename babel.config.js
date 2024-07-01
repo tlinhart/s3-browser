c18bfff537d2d60eac144461bcd59c7843024925
Tomáš Linhart <pasmen@gmail.com>
2024-07-01 18:28:15 +0200

    Add type tag to Babel config

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,4 @@
+/** @type {import("@babel/core").TransformOptions} */
 module.exports = {
   presets: ["@babel/preset-env", "@babel/preset-react"],
   plugins: [["@babel/plugin-transform-runtime", { regenerator: true }]],
