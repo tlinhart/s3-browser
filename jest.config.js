ad1707a17474d66e288c15e7f11a6ca91d1d5310
Tomáš Linhart <pasmen@gmail.com>
2024-07-01 12:01:33 +0200

    Move Babel configuration to JS file

--- jest.config.js
+++ jest.config.js
@@ -2,8 +2,9 @@
 module.exports = {
   rootDir: "src",
   testEnvironment: "jsdom",
-  moduleNameMapper: {
-    uuid: "uuid",
-  },
+  moduleNameMapper: { uuid: "uuid" },
   setupFiles: ["<rootDir>/tests/setup.js"],
+  transform: {
+    "^.+\\.(js|jsx)$": ["babel-jest", { rootMode: "upward" }],
+  },
 };
