a5e7ad87d113e2b8ba84a752c9c0a7b0610dbe16
Tomáš Linhart <pasmen@gmail.com>
2024-07-01 19:34:47 +0200

    Fix running Jest tests

--- jest.config.js
+++ jest.config.js
@@ -2,9 +2,9 @@
 module.exports = {
   rootDir: "src",
   testEnvironment: "jsdom",
-  moduleNameMapper: { uuid: "uuid" },
   setupFiles: ["<rootDir>/tests/setup.js"],
   transform: {
     "^.+\\.(js|jsx)$": ["babel-jest", { rootMode: "upward" }],
   },
+  transformIgnorePatterns: ["<rootDir>/node_modules/(?!(sinon)/)"],
 };
