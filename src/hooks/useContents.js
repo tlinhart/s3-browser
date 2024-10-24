83877d6c8f8f5545ed8051201cdc49006c40cdcf
Antoine James Tournepiche <antoinejt.serveur@gmail.com>
2024-10-24 23:33:35 +0200

    ordering

--- src/hooks/useContents.js
+++ src/hooks/useContents.js
@@ -3,11 +3,11 @@ import { S3Client, ListObjectsV2Command } from "@aws-sdk/client-s3";
 
 const s3Client = new S3Client({
   region: process.env.AWS_REGION,
-  endpoint: process.env.AWS_ENDPOINT_OVERRIDE || undefined,
   credentials: {
     accessKeyId: process.env.AWS_ACCESS_KEY_ID,
     secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
   },
+  endpoint: process.env.AWS_ENDPOINT_OVERRIDE || undefined,
   forcePathStyle: process.env.FORCE_PATH_STYLE === "true" || undefined,
 });
 
